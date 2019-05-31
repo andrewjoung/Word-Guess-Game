7c10f710bb08bf7bcd6c47939fd9d3b0294eec45
Andrew Joung <andrew.joung@gmail.com>
2019-05-30 17:12:06 -0700

    add more styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,20 +1,12 @@
+//Author: Andrew Joung
+//Date: May 30th 2019
+//THis is the JavaScript file that adds functionality to the Word Game web application
+//Keeps track of the game's state through an object and manipulates and updates the DOM 
+//according to the user input. 
+
 //An array of words that will be randomly chosen for the user to guess
 var wordArray = ["sword", "westeros", "esos", "bravos", "stark", "targaryen", "lannister", "direwolf", "dragon", "lion",
                  "gold", "shield", "winter", "throne", "king", "winterfell"];
-
-//An object that holds information about the state of the game
-var gameState = {
-    word: "",
-    wins: 0,
-    guessesLeft: 10,
-    lettersLeft: 1, //initalize to 1 so that the game doesnt think the user has won at the beginning of every new game
-    playing: false,
-    reset: function() {
-        this.word = "";
-        this.wins = 0;
-        this.guessesLeft = 10;
-    }
-};
 
 var startPrompt = document.getElementById("startPrompt");
 var winText = document.getElementById("winText"); //gets the text that displays the number of wins from html
@@ -24,13 +16,29 @@ var guessedLettersText = document.getElementById("lettersGuessed"); //div that d
 var resetButton = document.getElementById("resetButton"); //gets the reset button from html
 var image = document.getElementById("thronesImage"); //gets the image from html
 
+//An object that holds information about the state of the game
+var gameState = {
+    word: "", 
+    wins: 0, 
+    guessesLeft: 10,
+    lettersLeft: 1, //initalize to 1 so that the game doesnt think the user has won at the beginning of every new game
+    playing: false,
+    alreadyGuessed: [],
+    reset: function() { //resets the state of the game 
+        this.word = "";
+        this.wins = 0;
+        this.guessesLeft = 10;
+        this.alreadyGuessed = [];
+    }
+};
+
 //initalizes game
 function initalizeGame() {
     var randomNumber = Math.floor((Math.random() * wordArray.length));
     var randomWord = wordArray[randomNumber]; //picking our random word from the word array;
+    gameState.reset();
     gameState.word = randomWord;
     gameState.lettersLeft = randomWord.length;
-    gameState.guessesLeft = 10;
     startPrompt.style.display = "none";
     winText.textContent = gameState.wins.toString();
     guessText.textContent = gameState.guessesLeft.toString();
@@ -64,26 +72,31 @@ document.onkeyup = function(event) {
     if(!gameState.playing) {
        initalizeGame();
     } else { //when the game is started 
-        guessedLetter = event.key;
-        if(gameState.word.includes(guessedLetter)) { //if the user guesses the letter correctly         
-            //loop through the word and find every instance of the letter
-            //this ensures that letters that occcur more than once will appear on the screen
-           for(var i = 0; i < gameState.word.length; i++) {
-               if(gameState.word.charAt(i) === guessedLetter) { //if the letter at the current index is the correctly guessed letter 
-                var letter = document.getElementById("letter" + i);
-                letter.textContent = guessedLetter;
-                letter.style.border = "none";
-                gameState.lettersLeft--; //decrease the letters left to guess
-               }
-           }
-           thronesImage.style.display = "block";
-           thronesImage.src = "assets/images/correct.jpg";
-        } else { //the guess is incorrect 
-            guessedLettersText.innerHTML += "<div class=guessedLetterDiv>" + guessedLetter + "</div>";
-            gameState.guessesLeft--;
-            guessText.textContent = gameState.guessesLeft;
-            thronesImage.style.display="block";
-            thronesImage.src = "assets/images/incorrect.jpeg";
+        if(gameState.alreadyGuessed.indexOf(event.key) === -1) { //if the letter has not already been guessed
+            gameState.alreadyGuessed.push(event.key); //add the letter to already guessed array
+            guessedLetter = event.key;
+            if(gameState.word.includes(guessedLetter)) { //if the user guesses the letter correctly         
+                //loop through the word and find every instance of the letter
+                //this ensures that letters that occcur more than once will appear on the screen
+                for(var i = 0; i < gameState.word.length; i++) {
+                    if(gameState.word.charAt(i) === guessedLetter) { //if the letter at the current index is the correctly guessed letter 
+                        var letter = document.getElementById("letter" + i);
+                        letter.textContent = guessedLetter;
+                        letter.style.border = "none";
+                        gameState.lettersLeft--; //decrease the letters left to guess
+                    }
+                }
+                thronesImage.style.display = "block";
+                thronesImage.src = "assets/images/correct.jpg";
+            } else { //the guess is incorrect 
+                guessedLettersText.innerHTML += "<div class=guessedLetterDiv>" + guessedLetter + "</div>";
+                gameState.guessesLeft--;
+                guessText.textContent = gameState.guessesLeft;
+                thronesImage.style.display="block";
+                thronesImage.src = "assets/images/incorrect.jpeg";
+            }
+        } else {
+            alert("You already guessed this letter!"); //alert the user that the letter has been guessed already;
         }
     }
 
